13813c93375d195d957de16ce8dd0e6894b9d25c
vitor <vitorsalves0812@gmail.com>
2025-01-03 19:32:26 -0300

    transformado funcao em api

--- index.mjs
+++ index.mjs
@@ -3,8 +3,11 @@ import { log } from "./log.mjs";
 export const handler = async (event) => {
     log(event)
 
-    return{
+    return {
         statusCode: 200,
-        body: JSON.stringify(event),
-      };
+        body: `<html><body>Dados da requisicao ${JSON.stringify(event)}</body></html>`,
+        headers: {
+            "content-type": "text/html"
+        }
+    };
   };
